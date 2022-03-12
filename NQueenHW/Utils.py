2f8d740b23b7ae4d10d8b77161dd0bdb47797025
NinaM31 <57009004+NinaM31@users.noreply.github.com>
2022-03-12 10:59:19 +0300

    Removed nested forloop

--- NQueenHW/Utils.py
+++ NQueenHW/Utils.py
@@ -11,28 +11,25 @@ def expand(problem, c_board_state):
     # array of possible board state when adding 1 queen to c_board_state
     possible_board_state = []
 
-    # loop over the board
-    for i, row in enumerate(problem.board):
-        # 1st queen place only on 1st row, 2nd queen placed only in second row
-        if len(c_board_state) < i:
-            break
-        
-        for j, _ in enumerate(row):
-            # possible queen position
-            q_pos = (i, j) 
+    # 1st queen place only on 1st row, 2nd queen placed only in second row .. etc
+    i = len(c_board_state) # For optimization
 
-            # if the possible position is safe append it to c_board_state as possible board state
-            if problem.is_queen_safe(q_pos, c_board_state) and q_pos not in c_board_state:
-                # tmp is a single possible state
-                tmp = set()
+    for j, _ in enumerate(problem.board):
+        # possible queen position
+        q_pos = (i, j) 
 
-                # add current queen positions    
-                tmp |= c_board_state
+        # if the possible position is safe append it to c_board_state as possible board state
+        if problem.is_queen_safe(q_pos, c_board_state):
+            # tmp is a single possible state
+            tmp = set()
 
-                # add the new queen position
-                tmp.add(q_pos)
+            # add current queen positions    
+            tmp |= c_board_state
 
-                # append it to the list of possible states
-                possible_board_state.append(tmp)
+            # add the new queen position
+            tmp.add(q_pos)
+
+            # append it to the list of possible states
+            possible_board_state.append(tmp)
     
     return possible_board_state
